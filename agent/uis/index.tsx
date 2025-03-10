16beab0ebc78e61dcb4757601ff996d61b7e0d8e
bracesproul <braceasproul@gmail.com>
2025-03-10 10:42:09 -0700

    cr

--- agent/uis/index.tsx
+++ agent/uis/index.tsx
@@ -1,9 +1,7 @@
 import StockPrice from "./stockbroker/stock-price";
 import PortfolioView from "./stockbroker/portfolio-view";
 import AccommodationsList from "./trip-planner/accommodations-list";
-import BookAccommodation from "./trip-planner/book-accommodation";
 import RestaurantsList from "./trip-planner/restaurants-list";
-import BookRestaurant from "./trip-planner/book-restaurant";
 import BuyStock from "./stockbroker/buy-stock";
 import Plan from "./open-code/plan";
 import ProposedChange from "./open-code/proposed-change";
@@ -12,9 +10,7 @@ const ComponentMap = {
   "stock-price": StockPrice,
   portfolio: PortfolioView,
   "accommodations-list": AccommodationsList,
-  "book-accommodation": BookAccommodation,
   "restaurants-list": RestaurantsList,
-  "book-restaurant": BookRestaurant,
   "buy-stock": BuyStock,
   "code-plan": Plan,
   "proposed-change": ProposedChange,
