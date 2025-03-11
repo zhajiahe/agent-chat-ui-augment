76a4ffed202d693f201d2a61428ef4857163931d
bracesproul <braceasproul@gmail.com>
2025-03-11 13:38:25 -0700

    cr

--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,8 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      "@typescript-eslint/no-explicit-any": 0,
+      "@typescript-eslint/no-unused-vars": ["warn", { args: "none" }],
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
