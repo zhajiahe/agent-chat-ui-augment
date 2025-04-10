0eab726a0fb6cdedc082318cc5dba5367a0e1f15
bracesproul <braceasproul@gmail.com>
2025-04-10 12:51:29 -0700

    cr

--- src/app/api/[..._path]/route.ts
+++ src/app/api/[..._path]/route.ts
@@ -5,7 +5,7 @@ import { initApiPassthrough } from "langgraph-nextjs-api-passthrough";
 
 export const { GET, POST, PUT, PATCH, DELETE, OPTIONS, runtime } =
   initApiPassthrough({
-    apiUrl: process.env.LANGGRAPH_API_URL, // default, if not defined it will attempt to read process.env.LANGGRAPH_API_URL
-    apiKey: process.env.LANGSMITH_API_KEY, // default, if not defined it will attempt to read process.env.LANGSMITH_API_KEY
+    apiUrl: process.env.LANGGRAPH_API_URL ?? "remove-me", // default, if not defined it will attempt to read process.env.LANGGRAPH_API_URL
+    apiKey: process.env.LANGSMITH_API_KEY ?? "remove-me", // default, if not defined it will attempt to read process.env.LANGSMITH_API_KEY
     runtime: "edge", // default
   });
